8bcbb40a63058de65f97c4fefaeb43081b3bff98
ArshiaKhaund <56116485+ArshiaKhaund@users.noreply.github.com>
2026-02-17 10:39:44 +0530

    Update mylist.py

--- mylist.py
+++ mylist.py
@@ -1,1 +1,16 @@
-#Create any list
+from machine import Pin
+import time
+
+led1 = Pin(15, Pin.OUT )
+led2 = Pin(18, Pin.OUT )
+led3 = Pin(21,Pin.OUT )
+led4 = Pin(26, Pin.OUT )
+
+Arshialist = [[1,0,0,0],[0,1,0,0],[0,0,1,0],[0,0,0,1]]
+
+for j in Arshialist:
+        led1.value(j[0])
+        led2.value(j[1])
+        led3.value(j[2])
+        led4.value(j[3])
+        time.sleep(1)#Create any list
